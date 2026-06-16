8a2b0d484e377e4339dd45a1cbf7e8dd7a6f90a2
deva3654321 <deva3654321@gmail.com>
2026-06-16 21:53:01 +0530

    Fix Render port

--- backend.py
+++ backend.py
@@ -66,5 +66,5 @@ def text_to_speech():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
